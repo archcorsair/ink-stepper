2af4fc86f47b6af048585aacfc93efe92b6ccaa1
Daniel Shneyder <archcorsair@gmail.com>
2026-01-02 01:42:13 -0800

    feat: export useStepperContext hook and types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { Step } from "./Step";
 export { Stepper } from "./Stepper";
+export { useStepperContext } from "./StepperContext";
+export type { RegisteredStep, StepperContextValue } from "./StepperContext";
 export type {
   ProgressContext,
   StepContext,
