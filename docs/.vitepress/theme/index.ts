62fbb994fcb4bf51bf92b628d98b00eae9735ba5
Daniel Shneyder <archcorsair@gmail.com>
2026-01-02 14:44:45 -0800

    fix(docs): enable LLM copy button in navbar

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,17 @@
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
+// @ts-expect-error
+import CopyOrDownloadAsMarkdownButtons from "vitepress-plugin-llms/vitepress-components/CopyOrDownloadAsMarkdownButtons.vue";
+import { h } from "vue";
 import "virtual:group-icons.css";
 
 export default {
   extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      "nav-bar-content-after": () => h(CopyOrDownloadAsMarkdownButtons),
+    });
+  },
   enhanceApp({ app }) {
     // Custom enhancements can go here
   },
